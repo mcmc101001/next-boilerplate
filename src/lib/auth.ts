464e92c17282cd1716b926dffe898857c894226d
mcmc101001 <tohmingchun101001@gmail.com>
2024-03-24 16:03:06 +0800

    Setup database, authentication and T3 env

---
+++ src/lib/auth.ts
@@ -0,0 +1,52 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { Adapter } from "next-auth/adapters";
+import { prisma } from "./prisma";
+import { env } from "@/env";
+
+export const authOptions: NextAuthOptions = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id!;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
+});
